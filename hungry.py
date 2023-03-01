e8c479afa66aaca96e78abafb22f98260682c96a
EllevenLabs <ellevenindustries@gmail.com>
2023-03-01 19:04:32 +0300

    water

--- hungry.py
+++ hungry.py
@@ -1,5 +1,10 @@
 hungry=input("are you hungry?")
 if hungry=="yes":
-    print("eat Mutura and kichwa")
+    print("eat Mutura and Kichwa")
+    print("eat pizza")
+    print("eat burger")
 else:
+    thirsty=input("are you thirsty?")
+    if thirsty=="yes":
+        print("chukua chaser")
     print("go party and....")
